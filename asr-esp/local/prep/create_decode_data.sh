a9ea830404c3cd632c95dd449819f0c091512f02
teinhonglo <teinhonglo@gmail.com>
2023-01-03 21:27:27 -0500

    [Script] comestic for coding

--- asr-esp/local/prep/create_decode_data.sh
+++ asr-esp/local/prep/create_decode_data.sh
@@ -16,13 +16,15 @@ if [ $stage -le 0 ]; then
                 rm -rf $data_root/$test_set/$f;
             fi
         done
-        for wav_path in `find $data_dir -name "*.wav"`; do
+        
+        for wav_path in `find $data_dir/wavs/ -name "*.wav"`; do
             wav_fn=`basename $wav_path | cut -d"." -f1`
             phn_no=`echo $wav_fn | cut -d"-" -f1`
             echo "$wav_fn $wav_path" >> $data_root/$test_set/wav.scp
             echo "$wav_fn $phn_no" >> $data_root/$test_set/utt2spk
             echo "$wav_fn $phn_no" >> $data_root/$test_set/text
         done
+        
         utils/fix_data_dir.sh $data_root/$test_set
     done
 fi
